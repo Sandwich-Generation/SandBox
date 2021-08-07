d020e9e99caa757e7e86ed017529abedf37ff210
Davi koth <iurikoth@gmail.com>
2021-08-06 23:10:21 -0300

    Add VNode styler

---
+++ import.js
@@ -0,0 +1,12 @@
+node.setImage("smm");
+
+/* To create a VNode */
+vnd.node {
+  import nodejsvault from "node" {
+    import function vaultSet(mn, vnd, vnode);
+    import var loot;
+    import nodevault 49;
+  };
+  nodejsvault.loot("5");
+}
+vnode.default();
